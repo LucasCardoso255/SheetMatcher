2fa7407c5bcfce559c1d60e66ab72c5f61cc708e
lucascardoso255 <lucasrabellocardoso@gmail.com>
2025-07-29 16:11:52 -0300

    refatoração para adicionar colunas em conjuto a fins de comparação

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def ordenar_planilha_por_referencia(planilha1_path='planilha1.xlsx', planilha2_path='planilha2.xlsx', output_path='saida_ordenada.xlsx'):
+def criar_planilha_combinada(planilha1_path='planilha1.xlsx', planilha2_path='planilha2.xlsx', output_path='saida.xlsx'):
     df1 = pd.read_excel(planilha1_path)
     df2 = pd.read_excel(planilha2_path)
 
@@ -14,26 +14,19 @@ def ordenar_planilha_por_referencia(planilha1_path='planilha1.xlsx', planilha2_p
         print(f"Erro: Coluna '{coluna_produto_df2}' não encontrada em '{planilha2_path}'.")
         return
 
-    df2['chave_correspondencia'] = df2[coluna_produto_df2].astype(str).str.strip()
-    df1['chave_ordenacao'] = df1[coluna_produto_df1].astype(str).str.strip()
+    df1[coluna_produto_df1] = df1[coluna_produto_df1].astype(str).str.strip()
+    df2[coluna_produto_df2] = df2[coluna_produto_df2].astype(str).str.strip()
 
-    df_ordenado_lista = []
-    chaves_encontradas = set()
+    df_final = pd.merge(df1, df2,
+                        left_on=coluna_produto_df1,
+                        right_on=coluna_produto_df2,
+                        how='left',
+                        suffixes=('', '_df2'))
 
-    for chave_ordenacao in df1['chave_ordenacao']:
-        linha_correspondente = df2[df2['chave_correspondencia'] == chave_ordenacao]
-        if not linha_correspondente.empty:
-            df_ordenado_lista.append(linha_correspondente)
-            chaves_encontradas.add(chave_ordenacao)
-
-    df_ordenado = pd.concat(df_ordenado_lista, ignore_index=True) if df_ordenado_lista else pd.DataFrame(columns=df2.columns)
-
-    chaves_nao_encontradas = df2[~df2['chave_correspondencia'].isin(chaves_encontradas)]
-    
-    df_final = pd.concat([df_ordenado, chaves_nao_encontradas], ignore_index=True)
-
-    df_final = df_final.drop(columns=['chave_correspondencia'])
+    if coluna_produto_df2 + '_df2' in df_final.columns:
+        df_final = df_final.drop(columns=[coluna_produto_df2 + '_df2'])
 
     df_final.to_excel(output_path, index=False)
+    print(f"Arquivo '{output_path}' gerado com sucesso!")
 
-ordenar_planilha_por_referencia()
+criar_planilha_combinada()
